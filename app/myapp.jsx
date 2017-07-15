c042f5a9132a0a3820c479f0b55022ad71b765c7
pnegi <prinegi100@gmail.com>
2017-07-15 19:15:31 +0530

    improving code

--- app/myapp.jsx
+++ app/myapp.jsx
@@ -1,10 +1,45 @@
-class Hellocomponents  extends React.Component{
+// class Hellocomponents  extends React.Component{
 
-    render() {
-        return (
-            <div><h1>Hello World</h1></div>
+//     render() {
+//         return (
+//             <div><h1>Hello World</h1></div>
+//         );
+//     }
+
+// }
+// ReactDOM.render(<Hellocomponents/>, document.querySelector('#app'));
+class BussinessCard extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Name is {this.props.name}</h1>
+                <h2>Post is {this.props.post}</h2>
+                <p>Address is {this.props.address}</p>
+            </div>
         );
     }
-
 }
-ReactDOM.render(<Hellocomponents/>, document.querySelector('#app'));
+
+class CardHolders extends React.Component{
+    person ={
+        name:'anupriya',
+        post:'developer',
+        address:'noida'
+
+    };
+
+    render(){
+        return(
+            <div>
+                <h1>All members</h1>
+                <BussinessCard
+                name= {this.person.name}
+                post= {this.person.post}
+                address= {this.person.address}/>
+            </div>
+        )
+
+    }
+}
+
+ReactDOM.render(<CardHolders/>, document.querySelector('#app'));
